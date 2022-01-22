483c57e841fba74ce7316abe221a1a8ddbcf04ca
Swati-jain123 <98042616+Swati-jain123@users.noreply.github.com>
2022-01-22 12:19:49 +0530

    Add files via upload
    Day-3 Assignment

---
+++ Day_3.py
@@ -0,0 +1,11 @@
+# Input by user
+name=input("please enter your name : ")
+age=int(input("Please enter your age :"))
+salary=int(input("Please enter your salary :"))
+qua=input("Please enter your qualifications :")
+address=input("Please enter your address :")
+fm=input("Please enter your family members :")
+
+
+detail=[name,age,salary,qua,address,fm]
+print(f"The details are {detail}")
